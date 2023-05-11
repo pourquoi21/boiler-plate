6e1ff9fda74acb75ec5e38f734bce9553e886a87
pourquoi21 <miyaku219@naver.com>
2023-05-11 12:29:45 +0900

    Secutity

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jay:abcd1234@bolierplate.6tkztfz.mongodb.net/?retryWrites=true&w=majority",
-    {
-      // mongoose 6.0이상일 경우 아래의 코드가 필요없음
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    // mongoose 6.0이상일 경우 아래의 코드가 필요없음
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  // 회원가입시 필요한 정보를 client에서 가져오면
+  // 이를 데이터베이스에 넣어주어야 함.
+  const user = new User(req.body);
+
+  // legacy 코드
+  // user.save((err, userInfo) => {
+  //   if (err) return res.json({ success: false, err });
+  //   return res.status(200).json({
+  //     success: true,
+  //   });
+  // });
+
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
